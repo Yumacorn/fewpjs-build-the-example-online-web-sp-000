77267b407c9941eedb5f32b4930dd6a8cd807538
Yumacorn <ReachRyanMui@gmail.com>
2020-12-22 23:38:47 -0500

    commit tree 5e68528c1b5a1b1e2bef2ba2612ab89c36786003

--- main.js
+++ main.js
@@ -3,7 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const post = document.querySelectorAll(".media-post");
 
+// likeButton.addEventListener('click', function(event) {
+//     alert("Hello World!");
+// });
+
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   console.log('DOM fully loaded and parsed');
+// });
+
+window.addEventListener('click', (event) => {
+  console.log('Nah still bound');
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+   const likeButton = document.querySelectorAll(".like-glpyh");
+
+  likeButton.addEventListener('click', (e) => {
+    console.log(`${e} Nah still bound`);
+  }); 
+
+  if(e.target.className === "like-glyph"){
+    console.log(e.target);
+  };
+});
 
 
 
